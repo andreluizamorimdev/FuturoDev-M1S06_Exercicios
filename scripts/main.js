2b21f3cf2003596560579a1fef444f79f9d9b829
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2024-02-24 19:29:19 -0300

    Exercicio 08 - manipulação do Dom Adicionado

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,90 @@ function boasVindas() {
     alert('Bem-vindo(a) ao meu site!');
 }
 boasVindas();
+
+const menuLinks = document.querySelectorAll('.navbar-items a[href^="#"]');
+
+function getDistanceFromTheTop(element) {
+    const id = element.getAttribute("href");
+    return document.querySelector(id).offsetTop;
+}
+
+/* function nativeScroll(distanceFromTheTop) {
+    window.scroll({
+        top: distanceFromTheTop,
+        behavior: "smooth",
+    });
+} */
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
+    smoothScrollTo(0, distanceFromTheTop);
+}
+
+
+
+menuLinks.forEach((link)=> {
+   link.addEventListener("click", scrollToSection);
+});
+
+function smoothScrollTo(endX, endY, duration) {
+    const startX = window.scrollX || window.pageXOffset;
+    const startY = window.scrollY || window.pageYOffset;
+    const distanceX = endX - startX;
+    const distanceY = endY - startY;
+    const startTime = new Date().getTime();
+  
+    duration = typeof duration !== "undefined" ? duration : 400;
+  
+    const easeInOutQuart = (time, from, distance, duration) => {
+      if ((time /= duration / 2) < 1)
+        return (distance / 2) * time * time * time * time + from;
+      return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+    };
+  
+    const timer = setInterval(() => {
+      const time = new Date().getTime() - startTime;
+      const newX = easeInOutQuart(time, startX, distanceX, duration);
+      const newY = easeInOutQuart(time, startY, distanceY, duration);
+      if (time >= duration) {
+        clearInterval(timer);
+      }
+      window.scroll(newX, newY);
+    }, 1000 / 60);
+  }
+
+  const projetos = document.querySelectorAll('.projeto');
+
+  projetos.forEach(projeto => {
+    projeto.addEventListener('click', () => {
+        alert(projeto.getElementsByTagName('h3').item(0).innerText);
+    })
+  })
+
+function trocarTema() {
+    const body = document.body;
+
+    const isDarkTheme = body.classList.contains('dark-theme');
+
+    body.classList.toggle('dark-theme', !isDarkTheme);
+    
+}
+
+let botaoTopo = document.getElementById("botaoTopo");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    botaoTopo.style.display = "block";
+  } else {
+    botaoTopo.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
